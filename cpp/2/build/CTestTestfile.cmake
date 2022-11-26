e5d4efcb067b12d76457fadb109b89c654696215
koddev <cagcode@gmail.com>
2022-11-26 23:31:17 +0400

    cpp

---
+++ cpp/2/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /mnt/e/code/git/notes/cpp/2
+# Build directory: /mnt/e/code/git/notes/cpp/2/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
